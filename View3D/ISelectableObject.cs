4fd2d7afbd90ff1f450755386555caf7f8cbfe0a
Nick Boey <nboey@northrop.com.au>
2025-01-04 10:29:04 +1000

    Add ISelectableObject interface

---
+++ View3D/ISelectableObject.cs
@@ -0,0 +1,26 @@
+﻿using THREE;
+
+namespace View3D;
+
+/// <summary>
+/// Interface for 3D objects that can be selected.
+/// </summary>
+public interface ISelectableObject
+{
+    public Material BaseMaterial { get; set; }
+    public Material? HoverInactiveMaterial { get; set; }
+    public Material? HoverActiveMaterial { get; set; }
+    public Material? SelectedMaterial { get; set; }
+
+    public bool HoverEnabled { get; set; }
+    public bool SelectionEnabled { get; set; }
+
+    public bool IsHovered { get; set; }
+    public bool IsSelected { get; set; }
+
+    public void Hover();
+    public void Unhover();
+    
+    public void Select();
+    public void Deselect();
+}
